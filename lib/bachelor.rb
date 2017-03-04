9c5d75d439cdf03f55d5d17222993b96c39d8afd
github username <github email address>
2017-03-04 17:33:47 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,55 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+	data[season].each do |contestant_hash|
+		if contestant_hash["status"] == "Winner"
+			return contestant_hash["name"].split[0]
+		end
+	end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+	data.each do |season, contestants_array|
+		contestants_array.each do |contestant_hash|
+			if contestant_hash["occupation"] == occupation
+				return contestant_hash["name"]
+			end
+		end
+	end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+	count = 0
+
+	data.each do |season, contestants_array|
+		contestants_array.each do |contestant_hash|
+			if contestant_hash["hometown"] == hometown
+				count += 1
+			end
+		end
+	end
+
+	count
 end
 
 def get_occupation(data, hometown)
-  # code here
+	data.each do |season, contestants_array|
+		contestants_array.each do |contestant_hash|
+			if contestant_hash["hometown"] == hometown
+				return contestant_hash["occupation"]
+			end
+		end
+	end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+	age_total = 0.0
+	num_of_contestants = 0
+
+	data[season].each do |contestant_hash|
+		age_total += contestant_hash["age"].to_f
+		num_of_contestants += 1
+	end
+
+	average_age = (age_total / num_of_contestants).round
 end
